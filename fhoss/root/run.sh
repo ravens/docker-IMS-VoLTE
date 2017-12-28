6b90a8c6757b60ae5ec6595a8ca93df68a3dd64c
Yan Grunenberger <yan@grunenberger.net>
2017-12-28 19:33:32 +0100

    Advancing on HSS components. Not connecting yet to Kamailio P/S/I-CSCF

--- fhoss/root/run.sh
+++ fhoss/root/run.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
-sleep 50000
+#!/bin/bash
+
+# Starting HSS
+cd /opt/OpenIMSCore/FHoSS/deploy
+JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
+
+echo "Building Classpath"
+CLASSPATH=$CLASSPATH:log4j.properties:.
+for i in lib/*.jar; do CLASSPATH="$i":"$CLASSPATH"; done
+echo "Classpath is $CLASSPATH."
+
+$JAVA_HOME/bin/java -cp $CLASSPATH de.fhg.fokus.hss.main.HSSContainer $1 $2 $3 $4 $5 $6 $7 $8 $9
+
