a6f5c51e1e6a0128d4b9afa3d789dc0a456b40ed
Yan Grunenberger <yan@grunenberger.net>
2017-12-28 15:29:11 +0100

    Add Kamalio; support data base and initial fhoss compilation

---
+++ db/3-scscf.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE `scscf`;
+
+USE scscf;
+
+CREATE TABLE `version` (
+    `table_name` VARCHAR(32) NOT NULL,
+    `table_version` INT UNSIGNED DEFAULT 0 NOT NULL,
+    CONSTRAINT table_name_idx UNIQUE (`table_name`)
+);
+
+REPLACE INTO version (table_name, table_version) values ('contact','6');
+
+DROP TABLE IF EXISTS contact;
+CREATE TABLE `contact` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `contact` char(255) NOT NULL,
+  `params` varchar(255) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `received` varchar(255) DEFAULT NULL,
+  `user_agent` varchar(255) DEFAULT NULL,
+  `expires` datetime DEFAULT NULL,
+  `callid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `contact` (`contact`)
+);
+
+REPLACE INTO version (table_name, table_version) values ('impu','6');
+DROP TABLE IF EXISTS impu;
+CREATE TABLE `impu` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `impu` char(64) NOT NULL,
+  `barring` int(1) DEFAULT '0',
+  `reg_state` int(11) DEFAULT '0',
+  `ccf1` char(64) DEFAULT NULL,
+  `ccf2` char(64) DEFAULT NULL,
+  `ecf1` char(64) DEFAULT NULL,
+  `ecf2` char(64) DEFAULT NULL,
+  `ims_subscription_data` blob,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `impu` (`impu`)
+);
+
+REPLACE INTO version (table_name, table_version) values ('impu_contact','6');
+DROP TABLE IF EXISTS impu_contact;
+CREATE TABLE `impu_contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `impu_id` int(11) NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `impu_id` (`impu_id`,`contact_id`)
+);
+
+REPLACE INTO version (table_name, table_version) values ('subscriber','6');
+DROP TABLE IF EXISTS subscriber;
+CREATE TABLE `subscriber` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `watcher_uri` varchar(50) NOT NULL,
+  `watcher_contact` varchar(50) NOT NULL,
+  `presentity_uri` varchar(50) NOT NULL,
+  `event` int(11) NOT NULL,
+  `expires` datetime NOT NULL,
+  `version` int(11) NOT NULL,
+  `local_cseq` int(11) NOT NULL,
+  `call_id` varchar(50) NOT NULL,
+  `from_tag` varchar(50) NOT NULL,
+  `to_tag` varchar(50) NOT NULL,
+  `record_route` varchar(50) NOT NULL,
+  `sockinfo_str` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `watcher_uri` (`event`,`watcher_contact`,`presentity_uri`)
+);
+
+REPLACE INTO version (table_name, table_version) values ('impu_subscriber','6');
+DROP TABLE IF EXISTS impu_subscriber;
+CREATE TABLE `impu_subscriber` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `impu_id` int(11) NOT NULL,
+  `subscriber_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `impu_id` (`impu_id`,`subscriber_id`)
+);
---
+++ kamailio/root/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd /usr/local/src/kamailio-4.3/kamailio
+sed -i s/'# DBENGINE=MYSQL'/'DBENGINE=MYSQL'/g utils/kamctl/kamctlrc
+sed -i s/'# DBHOST=localhost'/'DBHOST=db'/g utils/kamctl/kamctlrc
+sed -i s/'# DBNAME=kamailio'/'DBNAME=kamailio'/g utils/kamctl/kamctlrc
+sed -i s/'# DBRWUSER="kamailio"'/'DBRWUSER="kamailio"'/g utils/kamctl/kamctlrc
+sed -i s/'# DBRWPW="kamailiorw"'/'DBRWPW="kamailiorw"'/g utils/kamctl/kamctlrc
+sed -i s/'# DBROUSER="kamailioro"'/'DBROUSER="kamailioro"'/g utils/kamctl/kamctlrc
+sed -i s/'# DBROPW="kamailioro"'/'DBROPW="kamailioro"'/g utils/kamctl/kamctlrc
+sed -i s/'# SIP_DOMAIN=kamailio.org'/'SIP_DOMAIN=kamailio.org'/g utils/kamctl/kamctlrc
+sed -i s/'# DBACCESSHOST=192.168.0.1'/'DBACCESSHOST=dockervolte_kamailio_1.dockervolte_default'/g utils/kamctl/kamctlrc
+
+# config file
+mkdir -p /usr/local/etc/kamailio/
+cp /usr/local/src/kamailio-4.3/kamailio/utils/kamctl/kamctlrc /usr/local/etc/kamailio/
+
+
+# create database
+#/usr/local/sbin/kamdbctl create
+
+# daemonize
+sleep 50
+kamailio -D
